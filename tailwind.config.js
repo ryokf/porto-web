f60e03a0366b41775881288c03c55aafcc094883
Ryo Khrisna Fitriawan <94428496+ryokf@users.noreply.github.com>
2022-10-29 17:18:18 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary1 : '#7A0BC0',
+        primary2 : '#FA58B6',
+        primary3 : '#270082',
+        dark: '#1A1A40',
+        secondary : '#94a3b8',
+      },
+        fontFamily: {
+        'sora': ['Sora', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
